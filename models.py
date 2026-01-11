b6bf3c3d946d9888b5c49c5f4fc33437334cd673
ReeceLuo <reeceluo101@gmail.com>
2026-01-11 15:29:55 -0600

    Used new sqlalchemy syntax/type hints

--- models.py
+++ models.py
@@ -1,26 +1,42 @@
-from sqlalchemy import Column, Integer, Double, String, Boolean, DateTime
-from sqlalchemy.orm import func
+from sqlalchemy import ForeignKey, DateTime, Mapped
+from sqlalchemy.orm import func, mapped_column, relationship
+from typing import Optional, List
+
 from config import DeclarativeBase
 
 
 # User database model
 class User(DeclarativeBase):
-    __tablename__ = "user"
+    __tablename__ = "user_table"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    # hashed_password = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_defualt=func.now(), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    # hashed_password: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
+    expenses: Mapped[List["Expense"]] = relationship(back_populates="user")
+    # "Expense" is a forward reference to the Expense model class
+    # "user" is the attribute name in the Expense class
 
 
 # Expense database model
 class Expense(DeclarativeBase):
-    __tablename__ = "expense"
+    __tablename__ = "expense_table"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
-    amount = Column(Double, nullable=False)
-    description = Column(String, nullable=True)
-    category = Column(String, nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"))
+    user: Mapped["User"] = relationship(back_populates="expenses")
+    # "expenses" is the attribute name in the User class 
+
+    amount: Mapped[float] = mapped_column(nullable=False)
+    description: Mapped[Optional[str]] = mapped_column(nullable=True)
+    category: Mapped[Optional[str]] = mapped_column(nullable=True)
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
