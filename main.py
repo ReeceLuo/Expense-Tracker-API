0bb0a632045b5407b3c42bd695472685947aadd2
ReeceLuo <reeceluo101@gmail.com>
2026-01-12 21:56:16 -0600

    add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from config import engine, DeclarativeBase
+from routes import ExpenseRoutes, UserRoutes
+
+DeclarativeBase.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Expense Tracker API",
+    description="An API for users to track their expenses",
+    version="1.0.0"
+)
+
+app.include_router(ExpenseRoutes.router, UserRoutes.router)
+
+@app.get("/")
+def root():
+    return {"message": "Expense Tracker API is running"}
